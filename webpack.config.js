08fab12aa597dee5de92f7b12954a9b6bbd90e3c
Samuel Silva <samuelsilvawb@gmail.com>
2021-05-19 21:32:00 +0100

    chore: Setup webpack to bundle package
    - Added webpack config.
    - Added build script.

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/BackToTop.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "back-to-top.js",
+    library: {
+      name: "backToTop",
+      type: "umd",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
