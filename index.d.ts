5a273da576d4aa98b3d7fd7e9d6850c344ca10d2
Samuel Silva <samuelsilvawb@gmail.com>
2021-06-01 20:31:28 +0100

    chore: Generate definition type

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+export default BackToTop;
+declare function BackToTop(props: any): JSX.Element;
+declare namespace BackToTop {
+  namespace propTypes {
+    const children: any;
+    const className: any;
+    const onClick: any;
+    const alwaysVisible: any;
+  }
+}
